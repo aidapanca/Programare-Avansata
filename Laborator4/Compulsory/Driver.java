8c405508ae1b6157278757211243d90cbcfee41f
aidapanca <125951219+aidapanca@users.noreply.github.com>
2024-03-20 15:34:22 +0200

    Add files via upload

---
+++ Laborator4/Compulsory/Driver.java
@@ -0,0 +1,21 @@
+/**
+ @author Pâncă Aida-Gabriela, A5
+ **/
+package Compulsory;
+
+public class Driver extends Person{
+    Driver(String name, int age, String destination)
+    {
+        super(name, age, destination);
+    }
+
+    public String getTypeOfPerson()
+    {
+        return "Driver";
+    }
+
+    @Override
+    public String toString() {
+        return "Driver's Name: " + getName() + ", " + "Age: " + getAge() + ", " + "Destination: " + getDestination()  + "\n";
+    }
+}
---
+++ Laborator4/Compulsory/Passenger.java
@@ -0,0 +1,23 @@
+/**
+ @author Pâncă Aida-Gabriela, A5
+ **/
+package Compulsory;
+
+public class Passenger extends Person implements Comparable<Passenger> {
+
+    Passenger (String name, int age, String destination){
+        super(name, age, destination);
+    }
+    public String getTypeOfPerson(){
+        return "Passenger";
+    }
+
+    @Override
+    public int compareTo(Passenger oPerson){
+        return this.getName().compareTo(oPerson.getName());
+    }
+    @Override
+    public String toString() {
+        return "Passenger's Name: " + getName() + ", " + "Age: " + getAge() + ", " + "Destination: " + getDestination() + "\n";
+    }
+}
